a6516092365cf000ce1d25fcea4ea8dc78061f35
Darío Scattolini <dario.scattolini@gmail.com>
2022-10-06 01:27:27 +0200

    convert UserSession to singleton object to make it mockable, add test for no user logged case

---
+++ src/main/kotlin/org/craftedsw/tripservicekata/user/UserSession.kt
@@ -0,0 +1,21 @@
+package org.craftedsw.tripservicekata.user
+
+import org.craftedsw.tripservicekata.exception.CollaboratorCallException
+
+/*class UserSession {
+
+    private constructor()
+
+    companion object {
+        @JvmStatic val instance = UserSession()
+    }
+
+    val loggedUser: User?
+        get() = throw CollaboratorCallException("UserSession.getLoggedUser() should not be called in an unit test")
+
+}*/
+
+object UserSession {
+    val loggedUser: User?
+        get() = throw CollaboratorCallException("UserSession.getLoggedUser() should not be called in an unit test")
+}
---
+++ src/test/kotlin/org/craftedsw/tripservicekata/trip/TripServiceTest.kt
@@ -0,0 +1,33 @@
+package org.craftedsw.tripservicekata.trip
+
+import io.mockk.*
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.catchThrowable
+import org.assertj.core.api.Condition
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException
+import org.craftedsw.tripservicekata.user.User
+import org.craftedsw.tripservicekata.user.UserSession
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class TripServiceTest {
+    lateinit var user: User
+    lateinit var userSession: UserSession
+
+    @BeforeEach
+    fun setUp() {
+        user = User()
+        mockkObject(UserSession)
+    }
+
+    @Test
+    fun `getTripsByUser throws UserNotLoggedInException if user is not logged in`() {
+        every { UserSession.loggedUser } returns null
+
+        val service = TripService()
+
+        val thrown = catchThrowable { service.getTripsByUser(user) }
+
+        assertThat(thrown).isInstanceOf(UserNotLoggedInException::class.java)
+    }
+}
